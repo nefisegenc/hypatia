b4f219e28fbd88b86079a036f902e7405e6f9049
Nefise Genc <129396814+nefisegenc@users.noreply.github.com>
2023-10-27 16:01:41 +0300

    Add files via upload

---
+++ ing_index.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    console.log('geldim')
+  };
+
+
+  let modal = document.getElementById("myModal"),
+ btn = document.getElementById("myBtn"),
+ span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
